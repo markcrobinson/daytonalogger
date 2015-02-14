4058b651ac9c7538b7625e8df1ee7ff9a985098e
markcrobinson <markcrobinson@gmail.com>
2015-02-14 17:09:38 +0000

    new file:   gps_csv.py

---
+++ gps_csv.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+# Written by Dan Mandle http://dan.mandle.me September 2012
+# License: GPL 2.0
+ 
+import os
+from gps import *
+from time import *
+import time
+import threading
+ 
+gpsd = None #seting the global variable
+ 
+os.system('clear') #clear the terminal (optional)
+ 
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpspoller.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+ 
+if __name__ == '__main__':
+  gpspoller = GpsPoller() # create the thread
+  try:
+    gpspoller.start() # start it up
+    while True:
+      os.system('clear')
+ 
+      print ' GPS reading'
+      print '----------------------------------------'
+      print 'latitude    ' , gpsd.fix.latitude
+      print 'longitude   ' , gpsd.fix.longitude
+      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      print 'altitude (m)' , gpsd.fix.altitude
+      print 'eps         ' , gpsd.fix.eps
+      print 'epx         ' , gpsd.fix.epx
+      print 'epv         ' , gpsd.fix.epv
+      print 'ept         ' , gpsd.fix.ept
+      print 'speed (m/s) ' , gpsd.fix.speed
+      print 'climb       ' , gpsd.fix.climb
+      print 'track       ' , gpsd.fix.track
+      print 'mode        ' , gpsd.fix.mode
+ 
+      time.sleep(0.1) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpspoller.running = False
+    gpspoller.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
