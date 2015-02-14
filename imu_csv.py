609d3731cb28b5ac2834e0e500feaa70e0040116
markcrobinson <markcrobinson@gmail.com>
2015-02-14 17:04:26 +0000

    xmodified:   imu_csv.py

--- imu_csv.py
+++ imu_csv.py
@@ -1,6 +1,5 @@
 import sys, getopt
 
-
 sys.path.append('.')
 import RTIMU
 import os.path
@@ -45,37 +44,6 @@ while True:
     delta = data["timestamp"] - start_time
     delta_centi = delta / 1000000.0
     print "%.2f,%.2f,%.2f,%.2f,%.1f,%.1f,%.1f" % (delta_centi,data["accel"][0],data["accel"][1],data["accel"][2],math.degrees(fusionPose[0]),math.degrees(fusionPose[1]),math.degrees(fusionPose[2]))
-#    print "start_time: %i" % start_time
-#    print "timestamp:  %i" % data["timestamp"]
-#    print "delta:      %.2f" % delta_centi
-
-#    print "accelValid: " + str(data["accelValid"])
-#    print "accel: " + str(data["accel"])
-#    print "accel x: " + str(data["accel"][0])
-#    print "accel y: " + str(data["accel"][1])
-
-#    print "compassValid: " + str(data["compassValid"])
-#    print "compass: " + str(data["compass"])
-
-#    print "fusionPoseValid: " + str(data["fusionPoseValid"])
-#    print "fusionPose: " + str(data["fusionPose"])
-#    print("r: %f p: %f y: %f" % (math.degrees(fusionPose[0]), math.degrees(fusionPose[1]), math.degrees(fusionPose[2])))
-#    print("r: %i p: %i y: %i" % (int(math.degrees(fusionPose[0])), int(math.degrees(fusionPose[1])), int(math.degrees(fusionPose[2]))))
-#    print("x: %f y: %f z: %f" % (x,y,z))
-
-#    print "fusionQPoseValid: " + str(data["fusionQPoseValid"])
-#    print "fusionQPose: " + str(data["fusionQPose"])
-
-#    print "gyroValid: " + str(data["gyroValid"])
-#    print "gyro: " + str(data["gyro"])
-
-#    print "pressureValid: " + str(data["pressureValid"])
-#    print("Pressure: %f" % (data["pressure"]))
-
-#    print "temperatureValid: " + str(data["temperatureValid"])
-#    print("Temperature: %f" % (data["temperature"]))
-
-    #time.sleep(poll_interval*1.0/1000.0)
     time.sleep(0.02)
 
 
